2b80c6fddfd6c2cb345766ad8a89fe0fc49a9028
yusrilMuh <yusril34@gmail.com>
2018-04-09 06:23:00 +0800

    Update sticky-navbar.js

--- sticky-navbar.js
+++ sticky-navbar.js
@@ -3,10 +3,10 @@ var navbar = document.getElementById("region-pre").childNodes[1];
 var sticky = 120;
 
 function myFunction() {
+  navbar.style.height = 'calc(100vh - (120px - ' + window.pageYOffset + 'px))';
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
-  } else {
+  }  else {
     navbar.classList.remove("sticky");
-    navbar.style.height = 'calc(100vh - (120px - ' + window.pageYOffset + 'px))';
   }
 }
